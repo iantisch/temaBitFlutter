234755f9e1cf62cbae21d7a6fa610ea85462ade5
iantisch <73883224+iantisch@users.noreply.github.com>
2022-02-07 20:59:54 +0200

    Код main

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import 'dart:async';
+
+// statefulwidget project, в нем виджеты имеют наследование
+void main() {
+  runApp(
+    MyFirstApp(),
+  );
+}
+
+class MyFirstApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyFirstAppState();
+  }
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  bool _loading = false;
+  double _progressValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent,
+        appBar: AppBar(
+          title: const Text("First App"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            child: _loading
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      LinearProgressIndicator(value: _progressValue),
+                      Text(
+                        '${(_progressValue * 100).round()}%',
+                        style: const TextStyle(color: Colors.red, fontSize: 25),
+                      ),
+                    ],
+                  )
+                : const Text(
+                    "Press button to download",
+                    style: TextStyle(color: Colors.red, fontSize: 25),
+                  ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
+          child: const Icon(Icons.cloud_download),
+        ),
+      ),
+    );
+  }
+
+  void _updateProgress() {
+    const oneSec = const Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer t) {
+      setState(() {
+        _progressValue += 0.2;
+
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          t.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
+  }
+}
